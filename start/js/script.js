02252dfd55afac5f322aa15a748971f89640cd10
deionizedoatmeal <ian.k.bania@gmail.com>
2020-07-24 15:14:28 -0700

    clock

--- start/js/script.js
+++ start/js/script.js
@@ -324,7 +324,7 @@ let CONFIG = {
   /**
    * Show a twenty-four-hour clock instead of a twelve-hour clock with AM/PM.
    */
-  twentyFourHourClock: true,
+  twentyFourHourClock: false,
 };
 
 // Get invertedColors preference from cookies
