1b2b0fec04781e6e4d4d73c3b58279c4d0707212
deionizedoatmeal <ian.k.bania@gmail.com>
2020-07-24 16:45:30 -0700

    noaa

--- laura/js/script.js
+++ laura/js/script.js
@@ -200,7 +200,7 @@ let CONFIG = {
       category: 'Outdoor',
       name: 'NOAA NWS',
       key: '',
-      url: 'https://forecast.weather.gov/MapClick.php?lat=44.053&lon=-123.1122&unit=0&lg=english&FcstType=graphical',
+      url: 'https://forecast.weather.gov/MapClick.php?lat=44.053&lon=-123.1122',
       color: 'linear-gradient(135deg, #7A5F00, #98ff98)',
       icon: 'noaa',
       quickLaunch: false,
